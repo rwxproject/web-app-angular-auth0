788b4385e088a48418e27022f3eab2737dcc6474
rwxproject <rwxproject@gmail.com>
2021-11-28 12:15:45 -0500

    cors update

--- server/index.js
+++ server/index.js
@@ -3,19 +3,19 @@ var path = require("path");
 // var cors = require("cors");
 var serveStatic = require("serve-static");
 
-// let config = {
-//   origin: ["https://rwx.auth0.com"],
-//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-//   allowedHeaders: "Content-Type,Authorization,x-user",
-//   exposedHeaders: "Content-Range,X-Content-Range,x-user",
-//   preflightContinue: true,
-//   optionsSuccessStatus: 204,
-//   credentials: true,
-// };
+var corsOptions = {
+  origin: ["https://rwx.auth0.com", "http://system1.app.rwx.systems"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+  allowedHeaders: "Content-Type,Authorization,x-user-id",
+  exposedHeaders: "Content-Range,X-Content-Range,x-user-id",
+  preflightContinue: true,
+  optionsSuccessStatus: 204,
+  credentials: true,
+};
 
 var app = express();
-// app.options("*", cors()); // include before other routes
-// app.use(cors(config));
+app.options("*", cors()); // include before other routes
+app.use(cors(corsOptions));
 
 app.use(serveStatic(path.join(__dirname, "./dist")));
 app.listen(3000);
